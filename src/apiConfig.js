390ab774560828c9dccdf98d22202f961539cb47
RandyD <rdsoundguy@gmai.com>
2022-08-12 18:21:55 -0400

    Change again

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://dropouttunes-backend.herokuapp.com/',
+	production: 'https://dropouttunes-backend.herokuapp.com',
 
 	development: 'http://localhost:8000',
 }
